f1cf3dcf03ab39351f2dc772aaac8c6161ff656c
tapan0810 <arceus0810@gmail.com>
2026-07-19 21:41:57 +0530

    Add project files.

---
+++ Console_App_3_LINQ/Linq/Linq_Queries.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_3_LINQ.Linq
+{
+    public class Linq_Queries
+    {
+
+    }
+}
